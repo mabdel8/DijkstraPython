3f5d2a2bcc7d515577fbc35ce99046071985b961
Mohamed Abdelmagid <mohamedabdelmagid@Mohameds-MacBook-Air.local>
2023-02-17 23:41:35 -0500

    added grid lines

--- pathfinding.py
+++ pathfinding.py
@@ -9,6 +9,31 @@ window_height = 500
 
 window = pygame.display.set_mode((window_width, window_height))
 
+
+columns = 25
+rows = 25
+
+box_width = window_width // columns
+box_height = window_height // rows
+
+grid = []
+
+class Box:
+  def __init__(self, i , j):
+    self.x = i
+    self.y = j
+
+  #draw method for each box, outlines each box with color
+  def draw(self, win, color):
+    pygame.draw.rect(win, color, (self.x * box_width, self.y * box_height, box_width - 2, box_height - 2))
+
+#creates the grid
+for i in range(columns):
+  arr = []
+  for j in range(rows):
+    arr.append(Box(i, j))
+  grid.append(arr)
+
 def main() :
   while True:
     for event in pygame.event.get():
@@ -18,6 +43,11 @@ def main() :
         sys.exit()
 
     window.fill((0, 0, 0))
+
+    for i in range(columns):
+      for j in range(rows):
+        box = grid[i][j]
+        box.draw(window, (20,20,20))
 
     pygame.display.flip()
 main()
